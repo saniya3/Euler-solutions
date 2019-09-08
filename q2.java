71b482b5219f9da24d5c061f0bf439ca0b03c208
saniya3 <53290877+saniya3@users.noreply.github.com>
2019-09-08 20:51:43 +0530

    Create q2.java

---
+++ q2.java
@@ -0,0 +1,20 @@
+public class q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=1, b=2, s=2, c=0;
+while(c<4000000)
+{
+	c=a+b;
+	if(c%2==0)
+	{
+		s=s+c;
+	
+	}
+	a=b;
+	b=c;
+}
+System.out.println(s);
+	}
+
+}
