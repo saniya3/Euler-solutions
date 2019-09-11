514b7131a45d561f9da7af1a02daa48f1b09d216
saniya3 <53290877+saniya3@users.noreply.github.com>
2019-09-11 17:52:14 +0530

    Update q6.java

--- q6.java
+++ q6.java
@@ -1,14 +1,9 @@
 public class q6 {
 
 	public static void main(String[] args) {
-int s1 = 0,s2=0;
-int n=100;
-for(int i=1;i<=n;i++)
-{
-	s1=(n*(n+1)*((2*n)+1))/6;
-	s2=(n*(n+1))/2;
-}
+int s1 = 0, s2=0;
+s1=(100*101*201)/6;
+s2=(100*101)/2;
 System.out.println((s2*s2)-s1);
 	}
-
 }
